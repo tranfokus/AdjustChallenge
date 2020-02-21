ebe109a5f9331a8b0d2fc6947a8baba32e9fce5f
tranfokus <61323355+tranfokus@users.noreply.github.com>
2020-02-21 16:46:19 +0100

    Task 1 python program

---
+++ test1.py
@@ -0,0 +1,16 @@
+# python program to print the number from 1 to 10 in random number
+# on-line test at https://repl.it/languages/python3
+import random
+
+in_list = [x for x in range(1,11)] # create a list with number from 1 to 10
+out_list = [] # define a list to store the result
+
+# move element at random position from in_list (until the list is empty) to the out_list
+while len(in_list) > 0:
+    random_postion = random.randint(0,len(in_list)-1)  
+    out_list.append(in_list[random_postion])            
+    del in_list[random_postion]
+
+# print the result from out_list one line  
+print(' '.join(map(str, out_list))) 
+
